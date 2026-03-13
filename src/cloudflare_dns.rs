82ad3c564e7569301b1ffa2ef011b2270c0016f2
Tony Watkins <tony@llifion.net>
2026-03-13 13:28:04 +0000

    info: initial commit

--- src/cloudflare_dns.rs
+++ src/cloudflare_dns.rs
@@ -1,3 +1,4 @@
+use reqwest::Response;
 use serde_json::json;
 
 pub async fn get_zone_id(api_token: &str, zone_name: &str) -> Result<String, reqwest::Error> {
@@ -34,9 +35,45 @@ pub async fn create_or_update_record(
     record_type: &str,
     zone_id: &str,
 ) -> Result<(), reqwest::Error> {
+
+    let res = dns_records(api_token, zone_id, record_name, record_type).await?;
+    if res.status().is_success() {
+        // Read the response body as a JSON value
+        let json = res.json::<serde_json::Value>().await?;
+        let records = json["result"].as_array().unwrap();
+        if !records.is_empty() && records[0]["content"] == ip {
+            println!("The record is already correct. No need to do anything here!\n{}", records[0]);
+            Ok(())
+        } else if records.is_empty() {
+            let res = create_dns_record(api_token, ip, zone_id, record_name, record_type).await?;
+            if res.status().is_success() {
+                println!("Created a new record\n{}", res.text().await?);
+                Ok(())
+            } else {
+                println!("Failed to create record.");
+                Err(res.error_for_status().unwrap_err())
+            }
+        } else {
+            let record_id = records[0]["id"].as_str().unwrap();
+            let res = update_dns_record(api_token, ip, zone_id, record_name, record_type, record_id).await?;
+            if res.status().is_success() {
+                println!("{}", res.text().await?);
+                Ok(())
+            }
+            else {
+                println!("Failed to update record.");
+                Err(res.error_for_status().unwrap_err())
+            }
+        }
+    } else {
+        Err(res.error_for_status().unwrap_err())
+    }
+}
+
+/// Get all DNS records in the zone matching the name and type
+async fn dns_records(api_token: &str, zone_id: &str, record_name: &str, record_type: &str) -> Result<Response, reqwest::Error> {
     let client = reqwest::Client::new();
 
-    // First we get all DNS records in the zone matching the name and type
     let url = format!(
         "https://api.cloudflare.com/client/v4/zones/{}/dns_records?name={}&type={}",
         zone_id, record_name, record_type
@@ -50,23 +87,57 @@ pub async fn create_or_update_record(
         .send().await?;
 
     if res.status().is_success() {
-        // Read the response body as a JSON value
-        let json = res.json::<serde_json::Value>().await?;
-        let records = json["result"].as_array().unwrap();
-        if !records.is_empty() && records[0]["content"] == ip {
-            println!(
-                "The record is already correct. No need to do anything here!\n{}",
-                records[0]
-            );
-            Ok(())
-        } else if records.is_empty() {
-            // We need to create the record
-            let client = reqwest::Client::new();
-            let post_url = format!(
-                "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
-                zone_id
-            );
-            let body = serde_json::json!({
+        Ok(res)
+    }
+    else {
+        Err(res.error_for_status().unwrap_err())
+    }
+}
+
+/// Create a new DNS record
+async fn create_dns_record(api_token: &str, ip: &str, zone_id: &str, record_name: &str, record_type: &str ) -> Result<Response, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let post_url = format!(
+        "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
+        zone_id
+        );
+    let body = serde_json::json!({
+        "type": record_type,
+        "name": record_name,
+        "content": ip,
+        "ttl": 1,
+        "proxied": true
+    });
+
+    println!("POST URL: {}\nPOST body: {}", post_url, body);
+
+    let res = client
+        .post(&post_url)
+        .header("Authorization", format!("Bearer {}", api_token))
+        .header("Content-Type", "application/json")
+        .json(&body)
+        .send().await?;
+    if res.status().is_success() {
+        Ok(res)
+    }
+    else {
+        Err(res.error_for_status().unwrap_err())
+    }
+}
+
+// TODO test this refactored version
+
+/// Update the existing DNS record
+async fn update_dns_record(api_token: &str, ip: &str, zone_id: &str, record_name: &str, record_type: &str, record_id: &str) -> Result<Response, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let patch_url = format!(
+        "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
+        zone_id,
+        record_id
+    );
+
+    // Send a PATCH request to the API endpoint
+    let body = json!({
                 "type": record_type,
                 "name": record_name,
                 "content": ip,
@@ -74,52 +145,18 @@ pub async fn create_or_update_record(
                 "proxied": true
             });
 
-            println!("POST URL: {}\nPOST body: {}", post_url, body);
+    println!("PATCH URL: {}\nPATCH body: {}", patch_url, body);
 
-            let res = client
-                .post(&post_url)
-                .header("Authorization", format!("Bearer {}", api_token))
-                .header("Content-Type", "application/json")
-                .json(&body)
-                .send().await?;
-
-            if res.status().is_success() {
-                println!("Created a new record\n{}", res.text().await?);
-                Ok(())
-            } else {
-                println!("Failed to create record.");
-                Err(res.error_for_status().unwrap_err())
-            }
-        } else {
-            // We need to put a new value in the record
-            let patch_url = format!(
-                "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
-                zone_id,
-                records[0]["id"].as_str().unwrap()
-            );
-
-            // Send a PATCH request to the API endpoint
-            let body = json!({
-                "type": record_type,
-                "name": record_name,
-                "content": ip,
-                "ttl": 1,
-                "proxied": true
-            });
-
-            println!("PATCH URL: {}\nPATCH body: {}", patch_url, body);
-
-            let res = client
-                .patch(&patch_url)
-                .header("Authorization", format!("Bearer {}", api_token))
-                .header("Content-Type", "application/json")
-                .json(&body)
-                .send().await?;
-
-            println!("{}", res.text().await?);
-            Ok(())
-        }
-    } else {
+    let res = client
+        .patch(&patch_url)
+        .header("Authorization", format!("Bearer {}", api_token))
+        .header("Content-Type", "application/json")
+        .json(&body)
+        .send().await?;
+    if res.status().is_success() {
+        Ok(res)
+    }
+    else {
         Err(res.error_for_status().unwrap_err())
     }
 }
