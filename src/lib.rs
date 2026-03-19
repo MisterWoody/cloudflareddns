bde4d0bcf1aff3fc8023b1c47a231799218f08ea
Tony Watkins <tony@llifion.net>
2026-03-19 17:10:41 +0000

    fix: separate out shutdown for cleaner design

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@ mod cloudflare_dns;
 mod config;
 mod external_ip;
 mod monitor;
+mod shutdown;
 
 use std::error::Error;
 use std::time::Duration;
 use tokio::time::sleep;
-use tokio::{signal};
 use monitor::check_ips_and_update_dns;
-use crate::config::Config;
+use config::Config;
+use shutdown::shutdown_signal;
 
 pub async fn run() -> Result<(), Box<dyn Error>> {
 
@@ -26,22 +27,22 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let hosts = conf.hosts();
     let zones = conf.zones();
     // Split the hosts and zones strings on the semicolon character into vectors.
-    let hosts_vec = hosts.split(';').collect::<Vec<_>>();
-    let zones_vec = zones.split(';').collect::<Vec<_>>();
+    let hosts = hosts.split(';').collect::<Vec<_>>();
+    let zones = zones.split(';').collect::<Vec<_>>();
 
     // If the lengths of hosts and zones not equal, return an error.
-    if hosts_vec.len() != zones_vec.len() {
+    if hosts.len() != zones.len() {
         let length_mismatch: Box<dyn Error> = String::from("Error: hosts and zones have different lengths. These need to match").into();
         return Err(length_mismatch);
     }
     // If either hosts or zones are empty then error
-    if hosts_vec.is_empty() || zones_vec.is_empty() {
+    if hosts.is_empty() || zones.is_empty() {
         let empty_values: Box<dyn Error> = String::from("Error: hosts and zones must both not be empty.").into();
         return Err(empty_values);
     }
 
     if repeat_interval == 0 {
-         match check_ips_and_update_dns(&api_token, &hosts_vec, &zones_vec, ipv4, ipv6).await {
+         match check_ips_and_update_dns(&api_token, &hosts, &zones, ipv4, ipv6).await {
             Ok(()) => {},
             Err(e) => eprintln!("{}", e)
         }
@@ -49,7 +50,7 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     else {
         let mut end:bool = false;
         loop {
-            match check_ips_and_update_dns(&api_token, &hosts_vec, &zones_vec, ipv4, ipv6).await {
+            match check_ips_and_update_dns(&api_token, &hosts, &zones, ipv4, ipv6).await {
                 Ok(()) => {},
                 Err(e) => eprintln!("{}", e)
             }
@@ -68,29 +69,4 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-// Graceful shutdown with ctrl-c
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
 }
